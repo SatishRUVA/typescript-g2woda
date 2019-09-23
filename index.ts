3c3b3f0b1e4719aa4121e238aa970342b1bd4fc7
SatishRUVA <satishsoft007@gmail.com>
2019-09-23 17:00:22 -0500

    1 file modified

--- index.ts
+++ index.ts
@@ -1,6 +1,38 @@
-// Import stylesheets
-import './style.css';
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+* {
+  box-sizing: border-box;
+}
 
-// Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+.column {
+  float: left;
+  width: 33.33%;
+}
+
+/* Clearfix (clear floats) */
+.row::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+</style>
+</head>
+<body>
+
+<h2>Images Side by Side</h2>
+<p>How to create side-by-side images with the CSS float property:</p>
+
+<div class="row">
+  <div class="column">
+    <h2>Images Side by Side</h2>
+	<p>How to create side-by-side images with the CSS float property:</p>
+  </div>
+  <div class="column">
+    <img src="img_forest.jpg" alt="Forest" style="width:100%">
+  </div>
+</div>
+
+</body>
+</html>
